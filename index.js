949b5a4a225f1c602554d290d850e46ba333ca2e
francisco de oliveira sousa <qwertycobain01@gmail.com>
2023-07-01 19:47:05 -0300

    iniciando projeto bancocn

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express"
+
+import {v4} from "uuid"
+
+
+const app = express()
+app.use(express.json())
+const costumers =[]
+
+app.post("/account", (req, res)=>{
+    const {cpf, name} = req.body
+    const id = v4()
+    costumers.push({
+        cpf,    
+        name,
+        id: id,
+        statement :[]
+    })
+    return res.status(201).send()
+
+
+
+
+})
+
+
+app.listen(3000)
